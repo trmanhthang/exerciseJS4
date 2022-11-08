e80e5df5c7fc8731c264adb521689528e5510b22
trmanhthang <bigzeen861998@gmail.com>
2022-11-08 09:55:23 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+
+
+function changeType() {
+    let Amount = document.getElementById('amount');
+    let fromC = document.getElementById('from');
+    let toC = document.getElementById('to');
+    if( fromC.value === 'VND') {
+        toC.value = 'USD'
+    } else {
+        toC.value = 'VND'
+    }
+}
+
 function convert() {
     let Amount = document.getElementById('amount');
-    let fromC = document.getElementById('from').value;
-    let toC = document.getElementById('to').value;
+    let fromC = document.getElementById('from');
+    let toC = document.getElementById('to');
     let resultsUSD = parseInt(Amount.value) / 23000;
     let resultsVND = parseInt(Amount.value) * 23000;
-
-    if(fromC == 'VND' && toC == 'USD') {
+    if(fromC.value == 'VND' && toC.value == 'USD') {
         document.getElementById('resultDiv').innerHTML = ' ' + resultsUSD + '$';
     }else if(fromC == 'USD' && toC == 'VND') {
         document.getElementById('resultDiv').innerHTML = ' ' + resultsVND + 'Đ';
